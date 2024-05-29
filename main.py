0f477e1fc96cc74ca7b88bf833fe5b70ffaf902b
Qianyu Huang <qianyuhuang0722@gmail.com>
2024-05-29 15:43:23 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+# data preprocessing 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('./pt2_regression/2_multipleLinearRegression/50_Startups.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+ct = ColumnTransformer([('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+x = np.array(ct.fit_transform(X=x))
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression() 
+regressor.fit(x_train, y_train)
+
+y_pred = regressor.predict(x_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), axis=1))
